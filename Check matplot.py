0868bc35f8197b7e21f808c16e593f19d6420696
GOWTHAMRAJ K <37334527+gowtham216@users.noreply.github.com>
2022-07-04 18:16:40 +0200

    Add files via upload

---
+++ Check matplot.py
@@ -0,0 +1,21 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import numpy as np
+import matplotlib.pyplot as plt
+
+# evenly sampled time at 200ms intervals
+t = np.arange(0., 5., 0.2)
+
+# red dashes, blue squares and green triangles
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+plt.show()
+SyntaxError: multiple statements found while compiling a single statement
+import matplotlib.pyplot as plt
+
+# evenly sampled time at 200ms intervals
+t = np.arange(0., 5., 0.2)
+
+# red dashes, blue squares and green triangles
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+plt.show()
+SyntaxError: multiple statements found while compiling a single statement
